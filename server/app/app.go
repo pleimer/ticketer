c600910e6c6f32866ca439ecede088f8856f5f50
Paul Leimer <pfbleimer@gmail.com>
2024-07-14 20:32:26 -0500

    fix dependancy tree

--- server/app/app.go
+++ server/app/app.go
@@ -1,8 +1,12 @@
 package app
 
 import (
-	"fmt"
+	echomiddleware "github.com/labstack/echo/v4/middleware"
+	middleware "github.com/oapi-codegen/echo-middleware"
 
+	"github.com/pleimer/ticketer/server/services"
+
+	"github.com/labstack/echo/v4"
 	"github.com/pleimer/ticketer/server/db"
 	"github.com/pleimer/ticketer/server/lib/once"
 	"github.com/pleimer/ticketer/server/repositories"
@@ -10,12 +14,14 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
+// App contains all application singletons and lazy loads the dependancy tree
 var App func() *app
 
 type app struct {
 	*loggerConfig
 	*dbConfig
 	*repositoriesConfig
+	*routerConfig
 }
 
 func init() {
@@ -24,15 +30,17 @@ func init() {
 	loggerConfig := loggerConfig{}
 	dbConfig := dbConfig{}
 	repositoriesConfig := repositoriesConfig{}
+	routerConfig := routerConfig{}
 
 	// setup the singleton dependancy tree
 	loggerConfig.init()
 	dbConfig.init(&loggerConfig)
 	repositoriesConfig.init(&dbConfig)
+	routerConfig.init(&loggerConfig)
 
 	App = func() *app {
 		once.Once(func() {
-			a = &app{&loggerConfig, &dbConfig, &repositoriesConfig}
+			a = &app{&loggerConfig, &dbConfig, &repositoriesConfig, &routerConfig}
 		})
 		return a
 	}
@@ -45,9 +53,7 @@ type loggerConfig struct {
 
 func (l *loggerConfig) init() {
 	l.Logger = func() *zap.Logger {
-
 		once.Once(func() {
-			fmt.Println("initialized logger")
 			config := zap.Config{
 				Encoding:         "json",
 				Level:            zap.NewAtomicLevelAt(zapcore.InfoLevel),
@@ -84,7 +90,6 @@ type dbConfig struct {
 func (d *dbConfig) init(loggerConfig *loggerConfig) {
 	d.DB = func() *db.DB {
 		once.Once(func() {
-			fmt.Println("initialized db")
 			d.db = db.NewDB(
 				loggerConfig.Logger(),
 			)
@@ -106,5 +111,48 @@ func (r *repositoriesConfig) init(dbConfig *dbConfig) {
 			)
 		})
 		return r.ticketsRepository
+	}
+}
+
+type routerConfig struct {
+	router *echo.Echo
+	Router func() *echo.Echo
+}
+
+func (r *routerConfig) init(loggerConfig *loggerConfig) {
+	r.Router = func() *echo.Echo {
+		once.Once(func() {
+
+			swagger, err := services.GetSwagger()
+			if err != nil {
+				panic(err)
+			}
+
+			swagger.Servers = nil
+
+			ticketsService := services.NewTickets()
+
+			r.router = echo.New()
+
+			r.router.Use(echomiddleware.RequestLoggerWithConfig(echomiddleware.RequestLoggerConfig{
+				LogURI:    true,
+				LogStatus: true,
+				LogValuesFunc: func(c echo.Context, v echomiddleware.RequestLoggerValues) error {
+					loggerConfig.Logger().Info("request",
+						zap.String("URI", v.URI),
+						zap.Int("status", v.Status),
+					)
+
+					return nil
+				},
+			}))
+
+			// might as well validate the requests agains the schema
+			r.router.Use(middleware.OapiRequestValidator(swagger))
+
+			services.RegisterHandlers(r.router, ticketsService)
+
+		})
+		return r.router
 	}
 }
--- server/cmd/start.go
+++ server/cmd/start.go
@@ -3,16 +3,8 @@ package cmd
 import (
 	"net"
 
-	"github.com/labstack/echo/v4"
-
-	echomiddleware "github.com/labstack/echo/v4/middleware"
-	middleware "github.com/oapi-codegen/echo-middleware"
-
-	"go.uber.org/zap"
-
 	"github.com/pleimer/ticketer/server/app"
 	"github.com/pleimer/ticketer/server/db"
-	"github.com/pleimer/ticketer/server/services"
 )
 
 type Start struct {
@@ -23,41 +15,12 @@ func (s *Start) Execute(args []string) error {
 
 	// routes
 
-	swagger, err := services.GetSwagger()
-	if err != nil {
-		panic(err)
-	}
+	app.App().DB().Open(s.DBConnectionConfig)
+	defer app.App().DB().Close()
 
-	swagger.Servers = nil
-
-	ticketsService := services.NewTickets()
-
-	e := echo.New()
-
-	e.Use(echomiddleware.RequestLoggerWithConfig(echomiddleware.RequestLoggerConfig{
-		LogURI:    true,
-		LogStatus: true,
-		LogValuesFunc: func(c echo.Context, v echomiddleware.RequestLoggerValues) error {
-			app.App().Logger().Info("request",
-				zap.String("URI", v.URI),
-				zap.Int("status", v.Status),
-			)
-
-			return nil
-		},
-	}))
-
-	// might as well validate the requests agains the schema
-	e.Use(middleware.OapiRequestValidator(swagger))
-
-	services.RegisterHandlers(e, ticketsService)
-
-	e.Logger.Fatal(e.Start(net.JoinHostPort("0.0.0.0", "8080")))
-
-	d := db.NewDB(app.App().Logger())
-
-	d.Open(s.DBConnectionConfig)
-	defer d.Close()
+	app.App().Logger().Sugar().Fatal(
+		app.App().Router().Start(net.JoinHostPort("0.0.0.0", "8080")),
+	)
 
 	return nil
 }
